78a82385c226680f3b82bd24bce6f6e2cade825e
Bauerrr <33778131+Bauerrr@users.noreply.github.com>
2023-12-18 15:08:07 +0100

    laby f

---
+++ lab-f/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {ItemsComponent} from "./items/items.component";
+import {UsersComponent} from "./users/users.component";
+import {StartComponent} from "./start/start.component";
+import {NotFoundComponent} from "./not-found/not-found.component";
+import {authGuard} from "./auth.guard";
+import {adminGuard} from "./admin.guard";
+
+const routes: Routes = [
+  { path: "items", component: ItemsComponent, canActivate: [authGuard] },
+  { path: "users", component: UsersComponent, canActivate: [adminGuard] },
+  { path: "", pathMatch: "full", component: StartComponent },
+  { path: "**", pathMatch: "full", component: NotFoundComponent },
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
